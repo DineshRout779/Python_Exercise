ecd26df5eac80d67e19052fd06bd8b82fb0bd397
DineshRout779 <rddinesh779@gmail.com>
2020-11-03 08:54:48 +0530

    added files

---
+++ built_in_functions.py
@@ -0,0 +1,11 @@
+length_str = len("python")
+type_str = type("python")
+print(length_str)
+print(type_str)
+# an example
+age = input("Enter age")
+age = int(age)
+if age < 21:
+    print("You are young!")
+else:
+    print("You are old")
---
+++ even_odd.py
@@ -0,0 +1,8 @@
+# program to check even or odd
+def even_odd(x):
+    if x%2 == 0:
+        print("even")
+    else:
+        print("odd")
+even_odd(2)
+even_odd(3)
