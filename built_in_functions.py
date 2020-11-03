84d9a81541e2b7087245b7b6b1658132ed5ce90a
DineshRout779 <rddinesh779@gmail.com>
2020-11-03 09:00:51 +0530

    minor changes

--- built_in_functions.py
+++ built_in_functions.py
@@ -1,8 +1,10 @@
+# print length of data
 length_str = len("python")
-type_str = type("python")
 print(length_str)
+# print type of data
+type_str = type("python")
 print(type_str)
-# an example
+# an example of input function
 age = input("Enter age")
 age = int(age)
 if age < 21:
